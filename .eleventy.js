33259665394c625a237f28e1261c7af74cac39d0
andyjaramillo <andy.jaramillo022@gmail.com>
2026-01-18 11:39:21 -0500

    please build

--- .eleventy.js
+++ .eleventy.js
@@ -23,7 +23,7 @@ module.exports = function(eleventyConfig) {
             input: "src",      
             includes: "_includes",      
             data: "_data",      
-            output: "_site"    
+            output: "_site" 
         }   
     };
 };
